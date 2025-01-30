7b3bc1f5acd4e3388784c285c47de76c188dd9f3
SIWON990327 <pos06058@naver.com>
2025-01-30 22:44:31 +0900

    [Style] : 코드 스타일 변경

--- src/main/java/KDT/Web_IDE/global/exception/GlobalExceptionHandler.java
+++ src/main/java/KDT/Web_IDE/global/exception/GlobalExceptionHandler.java
@@ -1,18 +1,18 @@
 package KDT.Web_IDE.global.exception;
 
-import KDT.Web_IDE.global.response.BaseResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import KDT.Web_IDE.global.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = {GlobalException.class})
-    protected BaseResponse handleCustomException(GlobalException e) {
-        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
-        return BaseResponse.onFailure(e.getErrorCode(), null);
-    }
-
+  @ExceptionHandler(value = {GlobalException.class})
+  protected BaseResponse handleCustomException(GlobalException e) {
+    log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+    return BaseResponse.onFailure(e.getErrorCode(), null);
+  }
 }
